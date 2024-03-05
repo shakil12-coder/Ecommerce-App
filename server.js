e0e7f9521588f2161dacb5e035c30c09d175c7ec
Shakil Ahmed <shakilahmed0888@gmail.com>
2024-03-05 11:25:11 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,51 @@
+import express from "express";
+import colors from "colors";
+import dotenv from 'dotenv'
+import morgan from "morgan";
+import connectDb from "./config/db.js";
+import authRoutes from "./routes/authRoute.js"
+import cors from 'cors'
+
+ 
+
+
+//configure env
+dotenv.config();
+
+//database config
+connectDb();
+
+
+//rest object
+const app = express();
+
+
+
+//middleware
+app.use(cors());
+app.use(express.json())
+app.use(morgan('dev'))
+
+
+
+
+//routes
+app.use('/api/v1/auth' , authRoutes);
+
+
+
+
+//rest api
+app.get("/", (req, res) => {
+  res.send("Welcome to ecommerce app");
+});
+
+
+
+
+//PORT
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`sever running on ${process.env.DEV_MODE} on port ${PORT}`.bgCyan);
+});
